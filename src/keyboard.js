1071bd89a60d4ba825b10f75805aa32073080ae1
piyoppi <3podvator@gmail.com>
2017-09-20 00:32:56 +0900

    add requestAnimationFrame

---
+++ src/keyboard.js
@@ -0,0 +1,9 @@
+export default class keyboard{
+    constructor(){
+        document.addEventListener( 'keydown', this._onKeyDown, false );
+    }
+
+    _onKeyDown(e){
+
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import display from "./display.js"
+import keyboard from "./keyboard.js"
 
 let elements = [
     {id: 0, x: 100, y:200, width: 150, height: 50, force: [], velocity: [], animate: null, color: 'black' },
@@ -6,6 +7,11 @@ let elements = [
 ];
 
 let disp = new display(document.getElementById('display'));
-disp.renderElements(elements);
 
+function render(){
+    disp.renderElements(elements);
+    console.log(`renderer`);
+    window.requestAnimationFrame(render);
+}
 
+window.requestAnimationFrame(render);
