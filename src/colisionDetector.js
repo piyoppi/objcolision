e710c91f9952cd1ff7df3a359fa2df1a7b864545
piyoppi <3podvator@gmail.com>
2017-09-21 01:26:10 +0900

    あたり判定部分がかきかけ

--- src/colisionDetector.js
+++ src/colisionDetector.js
@@ -17,16 +17,20 @@ export default class colisionDetector {
     }
 
     isColision(item){
-
     }
 
     convPositionToMortonNumber(position){
-        
+    }
+
+    convToMortonNumber(item){
+        let itemPosition = [ [Math.floor((item.position[0]) / this._lowLevelCellSize[0]), Math.floor((item.position[1]) / this._lowLevelCellSize[1])];
+                             [Math.floor((item.position[0] + item.width) / this._lowLevelCellSize[0]), Math.floor((item.position[1] + item.height) / this._lowLevelCellSize[1])] ];
+        let mortonNum = [ convPositionToMortonNumber(itemPosition[0], itemPosition[1]) ];
+
+        (mortonNum[0] ^ mortonNum[1])
     }
 
     registerItem(item){
-        let itemPosition = [ [Math.floor((item.position[0]) / this._lowLevelCellSize[0]), Math.floor((item.position[1]) / this._lowLevelCellSize[1])];
-                             [Math.floor((item.position[0] + item.width) / this._lowLevelCellSize[0]), Math.floor((item.position[1] + item.height) / this._lowLevelCellSize[1])] ];
         
     }
 }
