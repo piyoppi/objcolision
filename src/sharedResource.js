e83e0a6df47f19ab8f4da24cc7d2d47487f73ffc
Shohei Ogata <shohei.ogata@pepabo.com>
2017-10-05 09:56:27 +0900

    フレームレートをカウントする処理を追加

--- src/sharedResource.js
+++ src/sharedResource.js
@@ -13,6 +13,18 @@ function addFrictionEfficient(materialName1, materialName2, value){
     dynamicFrictionEfficient [`${materialName2}-${materialName1}`] = value;
 }
 
+let startTime = new Date().getTime();
+let frameCount = 0;
+let measuredFramerate = 0;
+function getFrameRate(){
+    let nowTime = new Date().getTime();
+    if( nowTime - startTime > 1000 ) {
+        measuredFramerate = frameCount;
+        frameCount = 0;
+    }
+    frameCount++;
+}
+
 let deltaTime = 0.016;
 
 export default{
@@ -21,7 +33,10 @@ export default{
     frameRate: 1.0 / deltaTime,
     dynamicFrictionEfficient: dynamicFrictionEfficient,
     addFrictionEfficient: addFrictionEfficient,
+    startTime: startTime,
+    measuredFramerate: measuredFramerate,
 }
 
 //摩擦係数を登録
 addFrictionEfficient('default', 'lift', 0.5);
+
